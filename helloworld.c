58a819937243575ef3b20bacb4d3359c858cf524
Bakul333 <bakulgupta333@gmail.com>
2017-02-27 21:59:18 +0530

    First program in c

---
+++ helloworld.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+ {
+ 	char str[100];
+ 	clrscr():
+ 	printf("Enter the string u wan to enter");
+ 	scanf("%s",&str);
+ 	printf("Thse string entered by the user is as follows:- %s",str);
+
+ getch();
+ }
